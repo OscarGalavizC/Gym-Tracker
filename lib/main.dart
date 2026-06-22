f5447c6a6a21bc44fa95be573aa604c8025f6002
OscarGalavizC <winglessclub292@gmail.com>
2026-06-22 17:51:39 -0600

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Gym Tracker",
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Gym Tracker")),
+        body: const Center(child: Text("Hola Gym")),
+      ),
+    );
+  }
+}
+
